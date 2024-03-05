39e0d0deb32edd023536f9585981531a09dc1cba
Steven Kühnl <stevnkuehnl@gmail.com>
2024-03-05 18:48:27 +0100

    Added Project Intro text

---
+++ src/Components/Projects/ProjectIntroduction/ProjectIntroduction.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import '../../Main.css'
+import './ProjectIntroduction.css'
+
+
+class ProjectIntroduction extends Component {
+    state = {}
+    render() {
+        return (<div className="ProjectIntroPos">
+            <h1 className="Consolas ProjectIntroText">Here you can take a look at my Projects</h1>
+        </div>       
+        );
+    }
+}
+
+export default ProjectIntroduction;
