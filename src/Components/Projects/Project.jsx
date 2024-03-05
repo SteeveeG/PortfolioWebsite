f212817e06b19dbe884a964ac96d90d08c699f64
Steven Kühnl <stevnkuehnl@gmail.com>
2024-03-05 18:50:49 +0100

    implemented new Features in Project still not Finished

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,22 +1,39 @@
-
 import React, { Component } from "react";
+import '../Main.css'
 import './Project.css'
 import img from "../../Assets/CodeProjektBeispiel.png"
+import GithubLogo from "./Assets/github-mark.svg"
+
 
 
 class Project extends Component {
     state = {}
     render() {
-        return (<li className="ProjectPos">
-                <div className="Wrapper">
-                     <img src={img} alt="project" className="ProjectPic"/>
+        const { header, description, link } = this.props;
+        return (
+            <div className="ProjectWrapper">
+                <div className="Projectdiv">
+                    <img className="ProjectImg" src={img} />
+                    <div className="ProjectDescription">
+                      
+                    </div>
                 </div>
-        </li>
-
-
-
+            </div>
         );
     }
 }
+{/* <div className="ProjectDescription">
+
+<h1 className="Segoe ProjectHeader">
+    {header}
+</h1>
+<p className="Consolas ProjectText">
+    {description}
+</p>
+<a href={link}>
+    <img src={GithubLogo} className="GithubLogo" alt="githublogo" />
+</a>
+</div> */}
+
 
 export default Project;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,19 @@ import React from "react"
 import Introduction from "../Components/Introduction/Introduction";
 import Navbar from "../Components/Navbar/navbar";
 import Project from "../Components/Projects/Project";
+import ProjectIntroduction from "../Components/Projects/ProjectIntroduction/ProjectIntroduction";
 import "./Home.css"
 export default function Home() {
     return (
         <div className="background">
-            <Navbar/>
-            <Introduction/>
-            <Project/>
+            <Navbar />
+            <Introduction />
+            <ProjectIntroduction />
+            <div className="Container">
+                <Project header="Die Überschrift" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" link="#" />
+                <Project header="Die Überschrift" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" link="#" />
+                <Project header="Die Überschrift" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" link="#" />
+            </div>
         </div>
     )
 }
