7e5dfdcda5ba7bc63d0d9a4c01d1500a132c310e
Steven Kühnl <stevnkuehnl@gmail.com>
2024-03-06 14:10:55 +0100

    Non Responsive Porject Preview

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -15,25 +15,21 @@ class Project extends Component {
                 <div className="Projectdiv">
                     <img className="ProjectImg" src={img} />
                     <div className="ProjectDescription">
-                      
+                        <div className="ProjectDescriptionWrapper">
+                            <h1 className="Segoe ProjectHeader">
+                                {header}
+                            </h1>
+                            <p className="Consolas ProjectText">
+                                {description}
+                            </p>
+                            <a href={link}>
+                                <img src={GithubLogo} className="GithubLogo" alt="githublogo" />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         );
     }
 }
-{/* <div className="ProjectDescription">
-
-<h1 className="Segoe ProjectHeader">
-    {header}
-</h1>
-<p className="Consolas ProjectText">
-    {description}
-</p>
-<a href={link}>
-    <img src={GithubLogo} className="GithubLogo" alt="githublogo" />
-</a>
-</div> */}
-
-
 export default Project;
