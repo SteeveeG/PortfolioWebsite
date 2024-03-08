45d02c9dbd58dab3f06f1d009b94a3ee2e0c5184
Steven Kühnl <stevnkuehnl@gmail.com>
2024-03-08 13:34:46 +0100

    Made change Language Work

--- src/Components/I18n/i18n.js
+++ src/Components/I18n/i18n.js
@@ -1,22 +1,65 @@
-import LanguageDetector from "i18next-browser-languagedetector";
-import global_de from "../../Translation/De/global.json"
-import global_en from "../../Translation/En/global.json"
-import i18n from 'i18next';
-import { initReactI18next } from "react-i18next"
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
-i18n.use(LanguageDetector).use(initReactI18next).init({
-     debug: true,
-     lng: "en",
-     resources: {
-          de: {
-               AboutMe: {
-                    Intro: "Hi, ich bin ein 19 jähriger Full stack Azubi Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
-               },
-          },
-          en: {
-               AboutMe: {
-                    Intro: "Hi, I am a 19 year old  full Stack Apprentice Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
-               },
-          }
-     }
-});
+const resources = {
+  en: {
+    translation: {
+      // Navbar
+      "NavAbout": "About",
+      "NavProjects": "Projects",
+      "NavContact": "Contact",
+      // Navbar
+
+      //Introduction
+      "IntroHeader": "Full stack Apprentice\nfrom Stuttgart",
+      "IntroText": "A full stack apprentice with a great passion for developing new solutions and intensive projects in every area of development",
+      //Introduction
+
+      //About Me
+      "AboutMeHeader": "About Me",
+      "AboutMeText": "hi, I am a 19 year old full stack apprentice Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet",
+      //About Me
+
+      //projects
+      "ProjectIntro": "Here you can take a look at my Projects",
+      //projects
+
+    },
+  },
+  de: {
+    translation: {
+      // Navbar
+      "NavAbout": "Über Mich",
+      "NavProjects": "Projekte",
+      "NavContact": "Kontakt",
+      // Navbar
+
+      //Introduction
+      "IntroHeader": "Full stack Azubi\naus Stuttgart",
+      "IntroText": "Ein Full-Stack-Azubi mit einer großen Leidenschaft für die Entwicklung neuer Lösungen und intensiver Projekte in allen Bereichen der Entwicklung",
+      //Introduction
+
+      //About Me
+      "AboutMeHeader": "Über Mich",
+      "AboutMeText": "Hi , ich bin ein 19 jähriger full stack azubi Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet",
+      //About Me
+
+      //projects
+      "ProjectIntro": "Hier können Sie einen Blick auf meine Projekte werfen",
+      //projects
+
+    },
+  },
+};
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,47 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 import Introduction from "../Components/Introduction/Introduction";
 import Navbar from "../Components/Navbar/navbar";
 import Project from "../Components/Projects/Project";
 import ProjectIntroduction from "../Components/Projects/ProjectIntroduction/ProjectIntroduction";
 import AboutMe from "../Components/AboutMe/AboutMe";
 import "./Home.css"
- 
-export default function Home() {
- 
+import "./GermanHome.css"
+import "./EnglishHome.css"
+import i18next from 'i18next';
+
+function Home() {
+    useEffect(() => {
+        function handleLanguageChange(lng) {
+
+            document.body.classList.remove('en', 'de');
+            document.body.classList.add(lng);
+        }
+
+        i18next.on('languageChanged', handleLanguageChange);
+
+        return () => {
+            i18next.off('languageChanged', handleLanguageChange);
+        };
+    }, []);
+    const [lang, setLang] = useState('en');
+
+    const changeLanguage = (lng) => {
+        setLang(lng);
+        i18next.changeLanguage(lng);
+    }
+
+    useEffect(() => {
+        changeLanguage(lang);
+    }, [lang]);
+
+
     return (
         <div className="background">
             <Navbar />
-            <Introduction />            
-            <AboutMe/>
+            <Introduction />
+            <AboutMe />
             <div className="Container">
-                <ProjectIntroduction /> 
+                <ProjectIntroduction />
                 <Project header="Die Überschrift" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" link="#" />
                 <Project header="Die Überschrift" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" link="#" />
                 <Project header="Die Überschrift" description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et" link="#" />
@@ -22,6 +49,6 @@ export default function Home() {
         </div>
     )
 }
-
+export default Home;
 
 
