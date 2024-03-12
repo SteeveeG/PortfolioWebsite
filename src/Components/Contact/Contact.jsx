9899263451e66f503f5fe0b99dd7355b60cf0642
Steven Kühnl <stevnkuehnl@gmail.com>
2024-03-12 14:31:53 +0100

    Contact implemented
    not fully Responsive
    not implemented in i18n

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,52 +1,61 @@
-import React, {react} from 'react';
-import {useTranslation} from "react-i18next";
+import React, { react } from 'react';
+import { useTranslation } from "react-i18next";
+import emailLogo from "../../Assets/Svg/email-8-svgrepo-com.svg"
+import locationLogo from "../../Assets/Svg/location-pin-alt-1-svgrepo-com(1).svg"
+import Arrow from "../../Assets/Svg/WhiteArrow.svg"
 import '../Main.css'
 import './Contact.css'
 
 
+
 function Contact() {
 
-
-    const {t, i18n} = useTranslation();
+    const { t, i18n } = useTranslation();
     return (<div className="ContactWrapper">
-    <h1 className="ContactHeader Segoe">Let's Talk ! :)</h1>
-            <form className="Form">
+ 
+            <form className="ContactForm">
+                <h1 className="ContactHeader Segoe">Let's Talk !</h1>
+                <p className="ContactUnderLine Consolas">Feel free to contact me :)</p>
                 <div className="InputWrapper">
                     <input type="text" id="input" name="input"
-                           className="InputContact Consolas" required/>
+                        className="InputContact Consolas" required />
                     <label className="label Consolas" htmlFor="input">Name</label>
-                    <span className="ContactSpan"/>
+                    <span className="ContactSpan" />
                 </div>
                 <div className="InputWrapper">
-                    <input type="email" id="input" name="input"
-                           className="InputContact Consolas" required/>
+                    <input type="text" id="input" name="input"
+                        className="InputContact Consolas" required />
                     <label className="label Consolas" htmlFor="input">E-mail</label>
-                    <span className="ContactSpan"/>
+                    <span className="ContactSpan" />
                 </div>
-                <div className="InputWrapper">
-                    <input type="textarea" id="input" name="input"
-                           className="InputContact Consolas" required/>
+                <div className="MessageContact">
+                    <textarea type="textarea" id="input" name="input"
+                        className="MessageInput Consolas" required />
                     <label className="label Consolas" htmlFor="input">Nachricht</label>
-                    <span className="ContactSpan"/>
+                    <span className="ContactSpan  " />
                 </div>
+
+                <button className="Submit Consolas">
+                    Submit
+                </button>
             </form>
-        </div>
+            <form className="InfoForm">
+                <h5 className="InfoHeader Segoe">Info</h5>
+                <div className="EmailContainer">
+                    <img src={emailLogo} className="EmailLogo" alt="email Logo" />
+                    <p className="Email Consolas">email@example.com</p>
+                </div>
+                <div className="LocationContainer">
+                    <img src={locationLogo} className="LocationLogo" alt="Location Logo" />
+                    <p className="Location Consolas">Stuttgart, baden-württemberg</p>
+                </div>
+                <p className="LocationTipp ">MY HOOD</p>
+                <img src={Arrow} className="ArrowContact" alt="" />
+            </form>
+   
+    </div>
     )
         ;
 
 }
-
-//
-// <div className="ContactWrapper">
-//     <input type="text" id="input" name="input"
-//            className="InputContact" required/>
-//     <label className="label" htmlFor="input">Text</label>
-//     <span className="ContactSpan"/>
-// </div>
-// <div className="ContactWrapper">
-//     <input type="text" id="input" name="input"
-//            className="InputContact" required/>
-//     <label className="label" htmlFor="input">Text</label>
-//     <span className="ContactSpan"/>
-// </div>
 export default Contact;
