72260ce5b609e45f0ac6aadefe63f300bf40e2cc
Steven Kühnl <stevnkuehnl@gmail.com>
2024-03-14 17:26:47 +0100

    new resources

--- src/Components/I18n/i18n.js
+++ src/Components/I18n/i18n.js
@@ -5,20 +5,21 @@ const resources = {
   en: {
     translation: {
       // Navbar
-      "NavAbout": "About",
-      "NavProjects": "Projects",
-      "NavContact": "Contact",
+      "About": "About",
+      "Projects": "Projects",
+      "Contact": "Contact",
       // Navbar
 
       //Introduction
-      "IntroHeader": "Full stack Apprentice\nfrom Stuttgart",
+      "IntroHeader1": "Full stack Apprentice\nfrom ",
+      "introHeader2": " Stuttgart",
       "IntroText": "A full stack apprentice with a great passion for developing new solutions and intensive projects in every area of development",
       //Introduction
 
       //About Me
       "AboutMeHeader": "About Me",
       "AboutMeText": "Hi, I am a 19 year old developer from the beautiful Swabian region. I've been programming and developing software solutions since I was 15. That's why I started an apprenticeship as an IT specialist in application development in 2022. My specialty is the backend, and I am also learning the frontend. My programming languages include C#, Python, html & css, Javascript and SQL",
-      "AboutMeContact":"Have I sparked your interest? - Feel free to write to me!",
+      "AboutMeContact": "Have I sparked your interest? - Feel free to write to me!",
       //About Me
 
       //projects
@@ -27,28 +28,38 @@ const resources = {
 
 
       //LanguageHelper
-      "LanguageHelperText": "mogsch ned mal mei webseid uf deutsch angucke ?! "
+      "LanguageHelperText": "mogsch ned mal mei webseid uf deutsch angucke ?! ",
       //LanguageHelper
+
+
+      //Contact
+      "ContactUnderline": "Feel free to contact me :)",
+      "ContactMessage": "Message",
+      "ContactSubmit": "Submit",
+      "InfoLocation": "Stuttgart south-germany",
+      "InfoTipp": "MY HOOD",
+      //Contact
 
     },
   },
   de: {
     translation: {
       // Navbar
-      "NavAbout": "Über Mich",
-      "NavProjects": "Projekte",
-      "NavContact": "Kontakt",
+      "About": "Über Mich",
+      "Projects": "Projekte",
+      "Contact": "Kontakt",
       // Navbar
 
       //Introduction
-      "IntroHeader": "Full stack Azubi\naus Stuttgart",
+      "IntroHeader1": "Full stack Azubi\naus ",
+      "introHeader2":" Stuttgart",
       "IntroText": "Ein Full-Stack-Azubi mit einer großen Leidenschaft für die Entwicklung neuer Lösungen und intensiver Projekte in allen Bereichen der Entwicklung",
       //Introduction
 
       //About Me
       "AboutMeHeader": "Über Mich",
       "AboutMeText": "Hi, ich bin ein 19 jähriger Entwickler aus dem schönen Schwabenland. Seitdem ich 15 bin beschäftige ich ich mit der Programmierung und Entwicklung von Software-Lösungen. Deshalb habe ich 2022 eine Ausbildung zum Fachinformatiker in der Anwendungsentwicklung begonnen. Mein Spezialgebiet ist das Backend, zusätzlich lerne ich das Frontend. Zu meinen Programmiersprachen zählen C#, Python , html & css , Javascript und SQL",
-      "AboutMeContact":"Hab ich dein Interesse geweckt? - Schreib mir doch gerne!",
+      "AboutMeContact": "Hab ich dein Interesse geweckt? - Schreib mir doch gerne!",
       //About Me
 
       //projects
@@ -56,9 +67,16 @@ const resources = {
       //projects
 
       //LanguageHelper
-      "LanguageHelperText": "For my English Speaking Homies"
+      "LanguageHelperText": "For my English Speaking Homies",
       //LanguageHelper
-
+    
+      //Contact
+      "ContactUnderline": "Fühl dich frei, mich zu kontaktieren :)",
+      "ContactMessage": "Nachricht",
+      "ContactSubmit": "Schicken",
+      "InfoLocation": "Stuttgart Baden-Württemberg",
+      "InfoTipp": "MEINE HOOD",
+      //Contact
 
     },
   },
