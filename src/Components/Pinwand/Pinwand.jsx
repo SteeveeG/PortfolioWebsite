9434b5bc55c0adbdb60c6d184f149b239e929c41
Steve g <stevnkuehnl@gmail.com>
2024-08-22 22:15:57 +0200

    Implemented new Package vite-plugin-svgr

--- src/Components/Pinwand/Pinwand.jsx
+++ src/Components/Pinwand/Pinwand.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useTranslation } from "react-i18next";
 import css from './Pinwand.module.css'
-
+import Pin from './Assets/Pin.svg';
 
 function Pinwand(){
     const { t } = useTranslation();
-     return (<div> Pinwand </div>)
+     return (<div className={css.container}>
+         <div className={css.headerwrap} >
+            <p className={css.header}>Skills</p>
+            <p className={css.headerunderline}>------</p>
+         </div>
+         <Pin  width="100" height="100" />
+
+
+
+
+     </div>)
 }
 
 export default Pinwand
