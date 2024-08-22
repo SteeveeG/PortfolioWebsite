3427a2a46bd8d4432f389369396a64648a754aee
Steve g <stevnkuehnl@gmail.com>
2024-08-22 21:52:02 +0200

    Added new Component Pinwand

---
+++ src/Components/Pinwand/Pinwand.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useTranslation } from "react-i18next";
+import css from './Pinwand.module.css'
+
+
+function Pinwand(){
+    const { t } = useTranslation();
+     return (<div> Pinwand </div>)
+}
+
+export default Pinwand
