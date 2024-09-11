46bf120f1c90540bca1c97a717f04816eb69fbf2
Steven Kühnl <stevnkuehnl@gmail.com>
2024-09-11 17:19:12 +0200

    Still working on Responsive off Pin wall

--- src/Components/Pinwand/Pinwand.jsx
+++ src/Components/Pinwand/Pinwand.jsx
@@ -19,14 +19,16 @@ function Pinwand() {
             <p className={css.header}>Skills</p>
             <p className={css.headerunderline}>------</p>
         </div>
+        
+
         <Pin className={css.skillpin} />
         <hr className={css.linetocsharp} />
         <p className={css.desktopheader}>Desktop:</p>
-       
-       <div className={css.csharpcontainer}> 
-        <Pin className={css.csharppin} />
-        <Csharplogo className={css.csharplogo} />
-       </div>
+
+        <div className={css.csharpcontainer}>
+            <Pin className={css.csharppin} />
+            <Csharplogo className={css.csharplogo} />
+        
         <div className={css.comments}>
             <div>
                 <p className={css.pro}>Pro</p>
@@ -37,13 +39,13 @@ function Pinwand() {
         </div>
         <p className={css.csharpsnippet}> var result = new List&lt;List&lt;int&gt;&gt;();<br />
             GetCombination(list, new List&lt;int&gt;(),<br /> k, result); return result; </p>
-
+            </div>
 
         <hr className={css.linetocplusplus} />
-        <div className={css.cpluscpluscontainer}> 
-        <Pin className={css.cpluspluspin} />
-        <CplusplusLogo className={css.cpluspluslogo} />
-        </div>
+        <div className={css.cpluscpluscontainer}>
+            <Pin className={css.cpluspluspin} />
+            <CplusplusLogo className={css.cpluspluslogo} />
+      
         <p className={css.cplusplussnippet}>
             cout &gt;&gt; "if you need help type help" &gt;&gt; endl;<br />
             do {'{'}<br />
@@ -55,12 +57,14 @@ function Pinwand() {
             break;<br />
             {'}'}
         </p>
-
+        </div>
         <p className={css.webheader}>Web:</p>
 
         <hr className={css.linetohtml} />
-        <Pin className={css.htmlpin} />
-        <img src={htmllogo} alt="htmllogo" className={css.htmllogo} />
+        <div className={css.htmlcontainer}>
+            <Pin className={css.htmlpin} />
+            <img src={htmllogo} alt="htmllogo" className={css.htmllogo} />
+         
         <p className={css.htmlsnippet}>
             &lt;!DOCTYPE html&gt;<br />
             &lt;html lang="en"&gt;<br />
@@ -74,61 +78,71 @@ function Pinwand() {
             &lt;/body&gt;<br />
             &lt;/html&gt;<br />
         </p>
-
+        </div>
         <hr className={css.linetojavascript} />
-        <Pin className={css.javascriptpin} />
-        <img src={javascriptlogo} alt="javascriptlogo" className={css.javascriptlogo} />
+        <div className={css.javascriptcontainer} >
+            <Pin className={css.javascriptpin} />
+            <img src={javascriptlogo} alt="javascriptlogo" className={css.javascriptlogo} />
+        
         <p className={css.javascriptsnippet}>useEffect(() ={'>'} {'{'}<br />
             function handleLanguageChange(lng) {'{'}<br />
             document.body.classList.remove('en', 'de');<br />
             document.body.classList.add(lng);<br />
             {'}'}{'}'}
         </p>
-
+        </div>
         <hr className={css.linetocssfromhtml} />
         <hr className={css.linetocssfromjavascript} />
         <div className={css.csscontainer}>
             <Pin className={css.csspin} />
             <img src={csslogo} alt="csslogo" className={css.csslogo} />
-        </div>
+        
         <p className={css.csssnippet}>.Greetings{'{'}<br />
             font-size: 4em;<br />
             color: pink;<br />
             position: relative;<br />
             {'}'}</p>
-
+            </div>
 
         <hr className={css.linetoreact} />
-        <Pin className={css.reactpin} />
-        <img src={reactlogo} alt="reactlogo" className={css.reactlogo} />
-        <p className={css.reactsnippet}>import React from 'react'</p>
+        <div className={css.reactcontainer}>
+            <Pin className={css.reactpin} />
+            <img src={reactlogo} alt="reactlogo" className={css.reactlogo} />
+       
 
+        <p className={css.reactsnippet}>import React from 'react'</p>
+        </div>
 
         <hr className={css.linetovite} />
-        <Pin className={css.vitepin} />
-        <Vitelogo className={css.vitelogo} />
+        <div className={css.vitecontainer}>
+            <Pin className={css.vitepin} />
+            <Vitelogo className={css.vitelogo} />
+      
         <p className={css.vitesnippet}>
              // https://vitejs.dev/config/<br />
             export default defineConfig({'{'}<br />
             plugins: [react()],<br />
             {'}'})
         </p>
-
+        </div>
         <hr className={css.linetosqlfromcsharp} />
         <hr className={css.linetosqlfromcplusplus} />
         <hr className={css.linetosqlfromjavascript} />
         <hr className={css.linetosqlfrompython} />
-        <Pin className={css.sqlpin} />
-        <img src={SqlLogo} alt="SqlLogo" className={css.sqllogo} />
+        <div className={css.sqlcontainer}>
+            <Pin className={css.sqlpin} />
+            <img src={SqlLogo} alt="SqlLogo" className={css.sqllogo} />
+        
         <p className={css.sqlsnippet}>
             Select * from Players inner join Board on Board.GameID = 1<br />
             inner join Settings on Settings.GameID = 1<br />
             left join Game on Game.GameID =1 where Players.GameID = 1
         </p>
-
-
-        <Pin className={css.pythonpin} />
-        <PythonLogo className={css.pythonlogo} />
+        </div>
+        <div className={css.pythoncontainer}>
+            <Pin className={css.pythonpin} />
+            <PythonLogo className={css.pythonlogo} />
+        
         <p className={css.pythonsnippet}> coordinates = ""<br />
             for y_points in range(9):<br />
             for i in range(4):<br />
@@ -137,7 +151,7 @@ function Pinwand() {
             coordinates += maybe_pos[i][0] + str(maybe_pos[i][number_index + 1]) + ", "<br />
             coordinates = coordinates[0:coordinates.__len__() - 2]<br />
             print(coordinates)</p>
-
+            </div>
         <hr className={css.linetoresumefromcplusplus} />
         <hr className={css.linetoresumefromsql} />
         <hr className={css.linetoresumefrompython} />
